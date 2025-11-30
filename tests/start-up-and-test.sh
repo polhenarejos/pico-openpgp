42a13b2777a1d641df76a46b47fa6ddfd2531d9e
Pol Henarejos <pol.henarejos@cttc.es>
2025-11-30 19:11:53 +0100

    Fix start-up test script
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/start-up-and-test.sh
+++ tests/start-up-and-test.sh
@@ -18,6 +18,7 @@ sleep 1
 rm -f memory.flash
 echo -n "Start Pico OpenPGP..."
 ./build_in_docker/pico_openpgp > /dev/null 2>&1 &
+PID=$!
 test $? -eq 0 && echo -n "." || fail
 sleep 1
 ATR="3b:da:18:ff:81:b1:fe:75:1f:03:00:31:f5:73:c0:01:60:00:90:00:1c"
@@ -26,5 +27,20 @@ grep -q "${ATR}" <<< $e && echo -n "." || fail
 test $? -eq 0 && echo -e "${OK}" || fail
 
 pytest tests -W ignore::DeprecationWarning
+
+echo -n "Stopping Pico OpenPGP..."
+kill "$PID" 2>/dev/null || true
+kill -9 "$PID" 2>/dev/null || true
+test $? -eq 0 && echo -e "${OK}" || fail
+sleep 1
+rm -f memory.flash
+echo -n "Start Pico OpenPGP..."
+./build_in_docker/pico_openpgp > /dev/null 2>&1 &
+PID=$!
+test $? -eq 0 && echo -n "." || fail
+sleep 1
+e=$(opensc-tool -an 2>&1)
+grep -q "${ATR}" <<< $e && echo -n "." || fail
+test $? -eq 0 && echo -e "${OK}" || fail
 
 ./tests/scripts/cli-test.sh
