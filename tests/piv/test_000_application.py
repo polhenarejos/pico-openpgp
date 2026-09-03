42411315942867365163ea88047c8d78618934a5
Pol Henarejos <pol.henarejos@cttc.es>
2026-09-03 23:23:52 +0200

    Add GET DATA unknown-object regression
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/piv/test_000_application.py
+++ tests/piv/test_000_application.py
@@ -14,5 +14,10 @@ def test_discovery_object(piv):
     assert Tlv.unpack(0x53, response).startswith(bytes.fromhex("4f0ba0000003080000100001005f2f024010"))
 
 
+def test_get_data_unknown_object_returns_file_not_found(piv):
+    request = Tlv(0x5C, b"\x5f\xc1\xff")
+    assert_apdu_error(lambda: piv.protocol.send_apdu(0, 0xCB, 0x3F, 0xFF, request), SW.FILE_NOT_FOUND)
+
+
 def test_unsupported_instruction_has_expected_status(piv):
     assert_apdu_error(lambda: piv.protocol.send_apdu(0, 0x00, 0, 0), SW.INVALID_INSTRUCTION)
